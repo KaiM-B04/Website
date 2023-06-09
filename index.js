c07f65809c7edef70844b686b700bef830c2a3b9
kai <kaim.kodehode@gmail.com>
2023-06-09 12:10:15 +0200

    parallax fixes

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+let myLet = 3;
+const myConst = 7;
+console.log(myLet + myConst);
+
+let myArray = ["bla", "blargh", "blaaa"];
+console.log(myArray);
+
+let myString = "Stringtheory";
+console.log(myString);
+
+let myBoolean = true;
+console.log(myBoolean);
